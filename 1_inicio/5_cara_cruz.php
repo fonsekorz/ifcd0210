cd8f4e829d338cc22b578768c40c4313b0e59625
fonsekorz <fonsekaov84@proton.me>
2025-02-24 19:49:20 +0100

    mejorado

--- 1_inicio/5_cara_cruz.php
+++ 1_inicio/5_cara_cruz.php
@@ -1,3 +1,4 @@
 <?php
 $num = rand(1, 100);
-if ($num > 50 ? "<span style='color: blue;'>Cara</span>" : "<span style='color: red;'>Cruz</span>");
+$resultado = $num > 50 ? "<span style='color: blue;'>Cara</span>" : "<span style='color: red;'>Cruz</span>";
+echo $resultado;
