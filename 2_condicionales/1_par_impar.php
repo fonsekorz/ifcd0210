f81ddb218964e65af4c141900ab69fa7e24c6c9c
fonsekorz <fonsekaov84@proton.me>
2025-02-25 17:02:23 +0100

    Correccion

--- 2_condicionales/1_par_impar.php
+++ 2_condicionales/1_par_impar.php
@@ -1,6 +1,6 @@
 <!-- Imprimir si un numero es par o impar -->
 <?php
-define("MIN", rand(1, 100));
-define("MAX", rand(1, 100));
+define("MIN", 1);
+define("MAX", 100);
 define("NUM", rand(MIN, MAX));
 echo (NUM % 2 == 0 ? "El número " . NUM . " es par." : "El número " . NUM . " es impar.");
