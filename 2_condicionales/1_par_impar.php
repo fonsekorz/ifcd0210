95bddd433b80ba96a8d468ffb22df49e587521a4
fonsekorz <fonsekaov84@proton.me>
2025-02-25 17:09:16 +0100

    actualizar

--- 2_condicionales/1_par_impar.php
+++ 2_condicionales/1_par_impar.php
@@ -3,4 +3,5 @@
 define("MIN", 1);
 define("MAX", 100);
 define("NUM", rand(MIN, MAX));
+echo (NUM . "<br>");
 echo (NUM % 2 == 0 ? "El número " . NUM . " es par." : "El número " . NUM . " es impar.");
