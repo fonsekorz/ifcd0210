c747ca4b4a4a0ec6b816ae25884fe72f623397a3
fonsekorz <fonsekaov84@proton.me>
2025-02-25 18:30:35 +0100

    Actualización

---
+++ 2_condicionales/2_wasd.php
@@ -0,0 +1,27 @@
+<?php
+/* 
+Crear un programa que emule los controles de un videojuego: si pulso 
+si pulso w:'adelante'
+si pulso a:'izqueirda'
+si pulso s:'abajo'
+si pulso d:'derecha'
+*/
+$tecla = "d";
+echo ("La tecla es:$tecla<br>");
+switch ($tecla) {
+    case "w":
+        echo ("Adelante");
+        break;
+    case "a":
+        echo ("Izquierda");
+        break;
+    case "d":
+        echo ("Derecha");
+        break;
+    case "s":
+        echo ("Abajo");
+        break;
+    default:
+        echo ("Tecla incorrecta");
+        break;
+}
