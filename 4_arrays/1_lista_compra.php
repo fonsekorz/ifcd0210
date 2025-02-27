28896b6c1f275b26962eb795b3ca6546902d00b8
fonsekorz <fonsekaov84@proton.me>
2025-02-27 17:06:25 +0100

    actualizacion del ejercicio 1

--- 4_arrays/1_lista_compra.php
+++ 4_arrays/1_lista_compra.php
@@ -1,7 +1,8 @@
 <?php
 /* Crear una lista de la compra, que contenga al menos 4 elementos
 Utilizar un var_dump() para ver su contenido, modificar el 3er elemento y volvais a mostrarlo con el var_dump() */
-$lista_compra = ["uno", "dos", "tres", "cuatro"];
+$lista_compra = ["manzanas", "peras", "naranjas", "plátanos"];
 var_dump($lista_compra);
+echo("<br>");
 $lista_compra[2] = "modificado";
 var_dump($lista_compra);
