e6c4af9241bde6cfba9cf6934897f7a91e29c983
fonsekorz <fonsekaov84@proton.me>
2025-02-27 19:50:12 +0100

    ejercicio 4 temperaturas

--- 4_arrays/4_temperaturas.php
+++ 4_arrays/4_temperaturas.php
@@ -10,3 +10,28 @@ Dado un array con la temperatura media de  cada día de la semana, sacar:
 
 
 $temperaturas = [12, 15, 13, 12, 16, 11, 13];
+$max = $temperaturas[0];
+$min = $temperaturas[0];
+$media = 0;
+// Imprimir los valores del array
+echo "Temperaturas: ";
+foreach ($temperaturas as $valor) {
+    echo  $valor . " ";
+}
+echo "<hr>";
+// Recorrer el array
+foreach ($temperaturas as $valor) {
+    // Hallar la temperatura media
+    $media += $valor / count($temperaturas);
+    // si lo que vale $valor es mayor que lo que vale $max
+    if ($valor > $max) {
+        // $max recoge ese valor de $valor
+        $max = $valor;
+    }
+    if ($valor < $min) {
+        $min = $valor;
+    }
+}
+echo "Temperatura máxima: " . $max . "<br>";
+echo "Temperatura mínima: " . $min . "<br>";
+echo "Temperatura media: " . round($media, 2) . "<br>";
