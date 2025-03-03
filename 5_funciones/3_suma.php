4266645d70c2b928cf97413b2ce3c4c9f5bef4c9
fonsekorz <fonsekaov84@proton.me>
2025-03-03 16:40:10 +0100

    ejercicio 3

---
+++ 5_funciones/3_suma.php
@@ -0,0 +1,19 @@
+<?php
+/* 
+    Crear una función suma que reciba dos números como parámetros
+    y muestra por pantalla su suma con un echo
+    Hacer otra versión de la función, pero esta vez tiene que devolver el valor
+ */
+function suma($num1, $num2)
+{
+    $suma = $num1 + $num2;
+    echo "La suma de $num1 y $num2 es: $suma";
+}
+suma(7, 14);
+echo "<hr>";
+function sumav2($num1, $num2)
+{
+    $suma = $num1 + $num2;
+    return $suma;
+}
+echo "La suma de 7 y 14 es: " . sumav2(7, 14);
