e89f51d8b32c284fdfcae338b01105c798831531
fonsekorz <fonsekaov84@proton.me>
2025-03-03 15:51:34 +0100

    ejercicio 6

---
+++ 4_arrays/6_temperaturas-matriz.php
@@ -0,0 +1,48 @@
+<?php
+
+/* 
+    Similar al ejercicio anterior, pero en vez de crear una temperatura por día, en dos: 
+    añadir una temperatura aleatoria entre 15-25 grados para la mañana (con la clave "mañana")
+    y otra para la tarde entre 5-15 (con la clave "tarde")
+    
+    De esta manera tendremos un array en el que podríamos acceder a la clave "lunes" 
+    y dentro del lunes podría acceder a "mañana" o "tarde" con y así para el resto de días de la semana
+
+    [
+        "lunes"=>[
+            "mañana"=> 17,
+            "tarde" => 8
+        ],    
+        "martes"=>[ ...],
+        ...
+    ]
+
+    Una  vez creado: 
+    - Probar a mostrar la temperatura del viernes por la tarde.
+    - Iterarlo y mostrar el texto "el lunes por la mañana hay 17ºC, y por la tarde 8ºC, el martes por la mañana..."
+
+*/
+$dias = [
+    "lunes",
+    "martes",
+    "miercoles",
+    "jueves",
+    "viernes",
+    "sabado",
+    "domingo"
+];
+
+$temperaturas = [];
+
+foreach ($dias as $valor) {
+    $temperaturas[$valor] = [
+        'mañana' => rand(15, 25),
+        'tarde' => rand(5, 15)
+    ];
+}
+
+// var_dump($temperaturas);
+
+foreach ($temperaturas as $dia => $temp) {
+    echo "el $dia por la mañana hay " . $temp['mañana'] . "º, y por la tarde " . $temp['tarde'] . "º<br>";
+}
