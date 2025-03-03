300aa340906e238300e07c1838beaff66ce52fe1
fonsekorz <fonsekaov84@proton.me>
2025-03-03 16:22:54 +0100

    ejercicio 1

---
+++ 5_funciones/1_hola_mundo.php
@@ -0,0 +1,12 @@
+<?php
+/* 
+    Crear una función que haga echo de "hola mundo!". Probar a llamarla múltiples veces
+*/
+function saludo()
+{
+    echo "hola mundo! ";
+}
+
+saludo();
+saludo();
+saludo();
