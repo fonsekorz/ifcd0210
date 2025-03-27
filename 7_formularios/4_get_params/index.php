2ce61cde53d44482c9e07b25fc03cb14d1d3d9e9
fonsekorz <fonsekaov84@proton.me>
2025-03-27 17:01:01 +0100

    hole

--- 7_formularios/4_get_params/index.php
+++ 7_formularios/4_get_params/index.php
@@ -8,3 +8,24 @@ y en caso de que se especifiquen mostrar la suma de ambos números utilizando un
 
 Pista: la función isset($variable), nos permite comprobar si una variable está definida
 */
+function suma($num1, $num2)
+{
+    return "La suma es: " . ($num1 + $num2);
+}
+
+
+if (isset($_GET['num1']) && isset($_GET['num2'])) {
+    $num1 = $_GET['num1'];
+    $num2 = $_GET['num2'];
+    echo "<p>primer número es: " . $num1 . "</p>";
+    echo "<p>segundo número es: " . $num2 . "</p>";
+    if ($num1 != "" || $num2 != "") {
+        // Comprobar que sean números
+    }elseif (is_numeric($num1) && is_numeric($num2)) {
+        echo "<h1>" . suma($num1, $num2) . "</h1>";
+    } else {
+        echo "<script>alert('Ingrese números válidos')</script>";
+    }
+} else {
+    echo "<script>alert('Ingrese números')</script>";
+}
